aaf4b02225b76e67d2c5601d8bc845cf33a4aeb8
shreyansh3010 <shreyanshjain.2015@vit.ac.in>
2020-05-24 18:50:14 +0530

    Add the new parameters to the json response

--- backend/controllers/trainController.js
+++ backend/controllers/trainController.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const jsonFormatterService = require('../services/jsonFormatterService');
+const stationService = require('../services/stationService');
+const _ = require('lodash');
 
 getTrainDetails = (train_no) => {
     return new Promise((resolve, reject) => {
@@ -25,7 +27,8 @@ getTrainStatus = (train_no, startDate) => {
             .then(function (response) {
                 jsonFormatterService.convertToJson(response.data.split('=')[1])
                     .then((jsonData) => {
-                        resolve(jsonData);
+                        var fomattedJson = fomateJson(jsonData);
+                        resolve(fomattedJson);
                     })
             })
             .catch(function (error) {
@@ -33,6 +36,29 @@ getTrainStatus = (train_no, startDate) => {
                 reject(error);
             })
     })
+}
+
+fomateJson = (jsonData) => {
+    var new_stations = [];
+    var curr_station = _.get(_.find(jsonData[0].stations, {
+        stnCode: jsonData[0].curStn
+    }), 'distance');
+    _.forEach(jsonData[0].stations, (station) => {
+        var new_station = station;
+        new_station.stationName = _.get(stationService.getStationName(station.stnCode), 'name');
+        if (curr_station > new_station.distance) {
+            new_station.status = 'passed'
+        }
+        else if(curr_station == new_station.distance){
+            new_station.status = 'current'
+        }
+        else{
+            new_station.status = 'not_passed'
+        }
+        new_stations.push(new_station);
+    })
+    jsonData[0].stations = new_stations;
+    return jsonData;
 }
 
 
